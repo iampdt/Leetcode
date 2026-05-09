abfc86a2a64aee7c98609e5f2253f6554a80313c
Paras Dev Tyagi <97456954+iampdt@users.noreply.github.com>
2026-05-09 17:56:20 +0530

    Time: 62 ms (42.38%), Space: 98.3 MB (85.87%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public: 
+    bool helper(vector<int>&time,int totalTrips,long long mid)
+    {
+        long long cnt = 0;
+        for(int i=0;i<time.size();i++)
+        {
+            cnt+=1LL*(mid/time[i]);
+            if(cnt>=totalTrips) return true;
+        }
+        return cnt >= totalTrips;
+    }
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        
+        long long low = 1;
+        long long high = 1e18;
+
+        while(low < high)
+        {
+            long long mid = low + (high-low)/2;
+            if(helper(time,totalTrips,mid)) high = mid;
+            else low = mid + 1;
+        }
+
+        return low;
+    }
+};
