fc5ad3652dcf71390f9e6e6d77256cd065d6212f
Paras Dev Tyagi <97456954+iampdt@users.noreply.github.com>
2026-05-10 23:31:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool helper(int n ,vector<int>&nums,int mid)
+    {
+       int cnt = 0;
+       for(int i=0;i<nums.size();i++)
+       {
+         if(nums[i]%mid == 0) cnt+= nums[i]/mid;
+         else cnt+= nums[i]/mid + 1;
+       }
+       return cnt <= n;
+    }
+    int minimizedMaximum(int n, vector<int>& nums) {
+        
+        int low = 1;
+        int high = accumulate(nums.begin(),nums.end(),0);
+
+        while(low < high)
+        {
+            int mid = low + (high - low)/2;
+            if(helper(n,nums,mid)) high = mid;
+            else low = mid + 1;
+        }
+        return low;
+    }
+};
