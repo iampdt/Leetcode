0c4b0f7cfbaec191cbba716df8838c656d375c81
Paras Dev Tyagi <97456954+iampdt@users.noreply.github.com>
2026-05-12 20:44:31 +0530

    Time: 0 ms (100%), Space: 8.1 MB (40.07%) - LeetHub

---
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool check(int n, int maxSum, int index, int mid) {
+        int leftLength = index;
+        int rightLength = n - index - 1;
+        long long leftSum = getSlopeSum(leftLength, mid);
+        long long rightSum = getSlopeSum(rightLength, mid);
+        long long totalSum = leftSum + mid + rightSum;
+
+        return totalSum <= maxSum;
+    }
+    long long getSlopeSum(int length, int mid) {
+        long long val = mid - 1;
+        long long fullTriangle = val * (val + 1) / 2;
+        if (length < val) {
+            // Case 1: The slope hits the boundary before reaching 0.
+            // It gets chopped off. Your exact logic applies here!
+            long long choppedOff = (val - length) * (val - length + 1) / 2;
+            return fullTriangle - choppedOff;
+        } else {
+            // Case 2: The slope reaches 0 naturally.
+            // Because we used the "maxSum - n" trick, the remaining spaces are
+            // just 0s.
+            return fullTriangle;
+        }
+    }
+    int maxValue(int n, int index, int maxSum) {
+        if(n == maxSum) return 1;
+        int low = 1;
+        int high = maxSum;
+
+        while (low < high) {
+            int mid = low + (high - low + 1) / 2;
+            if (check(n, maxSum - n,index, mid))
+                low = mid;
+            else
+                high = mid-1;
+        }
+        return low+1;
+    }
+};
+
+// nums[mid] = max.
+// smallest sum = 1 to mid => Left side Sum
+// right side after mid length = n-index
