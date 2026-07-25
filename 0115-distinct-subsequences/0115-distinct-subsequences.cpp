9aa30100dad515d912374cb00180d0adb59748c9
Paras Dev Tyagi <97456954+iampdt@users.noreply.github.com>
2026-07-25 16:57:11 +0530

    Time: 33 ms (38.6%), Space: 27.1 MB (53.71%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
+    int solve(int i,int j,string &s,string &t,vector<vector<int>>& dp)
+    {
+         if(j==t.size()) return 1;
+        if(i>=s.size() || j>t.size()) return 0;
+       
+        if(dp[i][j] != -1) return dp[i][j];
+        long long op1 = 1LL*solve(i+1,j,s,t,dp);
+        long long op2 = 0;
+        if(s[i] == t[j]) op2 = 1LL*solve(i+1,j+1,s,t,dp);
+        return dp[i][j] = 1LL*op1 + 1LL*op2;
+    }
     int numDistinct(string s, string t) {
         
         int n = s.length();
         int m = t.length();
 
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=m;j++)
-            {
-                if(i==0 && j!=0) dp[i][j] = 0;
-                else if(j==0) dp[i][j] = 1;
-
-                else
-                {
-                    dp[i][j] = dp[i-1][j];
-                    if(s[i-1] == t[j-1]) dp[i][j] = 1LL*dp[i][j] + 1LL*dp[i-1][j-1];
-                }
-            }
-        }
-        return (int)dp[n][m];
+        return int(solve(0,0,s,t,dp));
     }
 };
