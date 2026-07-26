6adff334da1677f3aa75cdc6b682f6a3d366d856
Paras Dev Tyagi <97456954+iampdt@users.noreply.github.com>
2026-07-26 19:54:07 +0530

    Time: 56 ms (43.3%), Space: 54.6 MB (17.99%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        
+        int n = s.length();
+        int m = p.length();
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+         dp[0][0] = 1;
+         for (int j = 1; j <= m; j++) {
+            if (p[j - 1] == '*') dp[0][j] = dp[0][j - 1];
+        }
+
+         for(int i=1;i<=n;i++)
+         {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1] == p[j-1] || p[j-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*') dp[i][j] = dp[i-1][j] || dp[i][j-1];
+            }
+         }
+         return dp[n][m];
+    }
+};
