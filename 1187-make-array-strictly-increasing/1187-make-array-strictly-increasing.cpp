18ca1851a529d5aba7dc5df427ccfcb4ca76cc2d
Paras Dev Tyagi <97456954+iampdt@users.noreply.github.com>
2026-08-05 23:55:44 +0530

    Time: 262 ms (78.02%), Space: 73.5 MB (79.03%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
-    int solve(int idx, int prev, map<pair<int, int>, int>& memo,
-              vector<int>& nums, vector<int>& temp) {
-        if (idx >= nums.size())
-            return 0;
-        if (memo.find({idx, prev}) != memo.end())
-            return memo[{idx, prev}];
-        int op1 = 1e9;
-        int op2 = 1e9;
-
-        auto it = upper_bound(temp.begin(), temp.end(), prev);
-        if (nums[idx] > prev)
-            op1 = 0 + solve(idx + 1, nums[idx], memo, nums, temp);
-        if (it != temp.end())
-            op2 = 1 + solve(idx + 1, *it, memo, nums, temp);
-       return memo[{idx, prev}] = min(op1, op2);
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        sort(arr2.begin(), arr2.end());
+        arr2.erase(unique(arr2.begin(), arr2.end()), arr2.end());
+        unordered_map<int, int> dp;
+        
+        dp[-1] = 0; 
+        
+        for (int i = 0; i < arr1.size(); i++) {
+            unordered_map<int, int> next_dp;
+            
+            for (auto it : dp) {
+                int prev_val = it.first;
+                int ops = it.second;
+                if (arr1[i] > prev_val) {
+                    if (next_dp.find(arr1[i]) == next_dp.end()) {
+                        next_dp[arr1[i]] = ops;
+                    } else {
+                        next_dp[arr1[i]] = min(next_dp[arr1[i]], ops);
+                    }
+                }
+                auto upper = upper_bound(arr2.begin(), arr2.end(), prev_val);
+                if (upper != arr2.end()) {
+                    int replacement_val = *upper;
+                    if (next_dp.find(replacement_val) == next_dp.end()) {
+                        next_dp[replacement_val] = ops + 1;
+                    } else {
+                        next_dp[replacement_val] = min(next_dp[replacement_val], ops + 1);
+                    }
+                }
+            }
+            if (next_dp.empty()) {
+                return -1;
+            }
+            
+            dp = next_dp;
+        }
+        
+        int min_ops = 1e9;
+        for (auto it : dp) {
+            min_ops = min(min_ops, it.second);
+        }
+        
+        return min_ops;
     }
-    int makeArrayIncreasing(vector<int>& nums, vector<int>& arr2) {
-        set<int> st;
-        vector<int> temp;
-        for (int i = 0; i < arr2.size(); i++)
-            st.insert(arr2[i]);
-        for (auto it : st)
-            temp.push_back(it);
-        map<pair<int, int>, int> memo;
-        // for(int i=0;i<n;i++)
-        // {
-        //    for(int j=0;j<m;j++)
-        //    {
-        //      dp[i][j] =
-        //    }
-        // }
-        // for(int i=0;i<n;i++) cout<<dp[i]<<" ";
-        // return dp[n-1];
-        int res = solve(0, -1, memo, nums, temp);
-        return res >= 1e9 ? -1 : res;
-    }
-}; //
+};
