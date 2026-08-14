0493dbe23245d5b895e17b7576d1111c4ab628f2
Paras Dev Tyagi <97456954+iampdt@users.noreply.github.com>
2026-08-14 20:06:20 +0530

    Time: 7 ms (30.57%), Space: 19.5 MB (24.75%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        
+        int n = piles.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for (int i = 0; i < n; i++) dp[i][i] = piles[i];
+        for(int len = 2;len<=n;len++)
+        {
+            for(int i=0;i+len-1<n;i++)
+            {
+                int j = len+i-1;
+                dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1] > 0 ? true : false;
+    }
+};
