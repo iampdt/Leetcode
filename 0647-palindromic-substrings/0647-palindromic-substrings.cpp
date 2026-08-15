cd515a9c7402ff35c2e2ea88dd2ac2bfbc850bc0
Paras Dev Tyagi <97456954+iampdt@users.noreply.github.com>
2026-08-16 00:38:59 +0530

    Time: 30 ms (32.42%), Space: 34.8 MB (11.86%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int expandAroundIndex(string &s , int left , int right){
-        int count = 0 ;
-        while(left >= 0 && right < s.size()){
-            if(s[left] == s[right]){
-                count++;
-            }else{
-                break;
-            }
-            left--;
-            right++;
-        }
-        return count ;
-    }
-
     int countSubstrings(string s) {
-        int count = 0 ;
-        for(int i = 0 ; i < s.size() ; i++){
-            int oddCount = expandAroundIndex(s , i , i);
-            count = count + oddCount ;
-            int evenCount = expandAroundIndex(s , i , i+1);
-            count = count + evenCount ;
+       
+       int n = s.length();
+       vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+       for(int i=0;i<n;i++) dp[i][i] = 1;
+       int count = 0;
+
+       for(int len=2;len<=n;len++)
+       {
+        for(int i=0;i+len-1<n;i++)
+        {
+            int j = len+i-1;
+            // Transition 
+            if(len == 2 && s[i] == s[j]) dp[i][j] = 1;
+            else if(s[i] == s[j] && dp[i+1][j-1] == 1) dp[i][j] = 1;
         }
-        return count ;
+       }
+       for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<n;j++)
+        {
+            count += dp[i][j];
+        }
+       }
+      return count;
     }
 };
